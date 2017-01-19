e0522b5ba351940e038d34e663ff0b5b7de8079e
darlinabreu <daabreu@cristoreyny.org>
2017-01-19 13:23:06 +0000

    my comment

--- script.js
+++ script.js
@@ -4,19 +4,14 @@ $(document).ready(function() {
         var part1 = "https://api.apixu.com/v1/current.json?key=2cd86bbae0d64758aa6131339171201&q=";
         var url = part1 + name;
         $.getJSON(url, function(response) {
-            //$("#weather-results").append(response.current.temp_f);
-            //$("#weather-results").append(response.current.wind_mph);
-            //$("#weather-results").append(response.current.humidity);
-            var humidity = response.current.humidity;
-            //console.log(humid)
-            var temp = response.current.temp_f
-            //console.log(weather)
-            var wind = response.current.wind_mph
-            //console.log(wind)
-            $("#weather-results").append("<li>" + "The wind speed is" + " " + wind + "</li>")
-            $("#weather-results").append("<li>" + "The weather is" + " " + temp + "</li>")
-            $("#weather-results").append("<li>" + "The humidity is" + " " + humidity + "</li>")
-
+            $("#weather-results").append('The temp is' + " " + response.current.temp_f + "<br>");
+            $("#weather-results").append("The wind speed is" + ' '+ response.current.wind_mph + "<br>");
+            $("#weather-results").append("The humidity is" + " " + response.current.humidity + "<br>");
+         $("#weather-results").css('background-color', 'red');
         });
     });
 });
+
+
+
+
